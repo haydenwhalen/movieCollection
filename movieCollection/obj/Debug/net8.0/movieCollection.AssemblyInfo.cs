e91950745235d782fa31535b66b7c3aecb838980
haydenwhalen <153124475+haydenwhalen@users.noreply.github.com>
2025-02-17 15:50:59 -0700

    4

--- movieCollection/obj/Debug/net8.0/movieCollection.AssemblyInfo.cs
+++ movieCollection/obj/Debug/net8.0/movieCollection.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movieCollection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386eb591f5eec78f0df65899e7ccba8cdf1821db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+576b83b217c851c0f1c39aa028a1d6bfdac8fcfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("movieCollection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movieCollection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
